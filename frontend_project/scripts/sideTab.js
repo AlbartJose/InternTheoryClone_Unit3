9bb70179ddab40c7fc68805bce659ec2fef32229
Srikanth <srikanth.vs12@gmail.com>
2021-08-27 20:15:57 +0530

    logout function fixed

--- frontend_project/scripts/sideTab.js
+++ frontend_project/scripts/sideTab.js
@@ -1,6 +1,8 @@
 
 var i = 0;
 var j = 0;
+var k = 1;
+var l = 0;
 
 function toggleRegister() {
     i++;
@@ -27,4 +29,50 @@ function toggleLogin() {
         div1.style.display = "none";
         div2.style.display = "none";
     }
+}
+function login() {
+    let login_div = document.getElementById("signin");
+    login_div.innerHTML = null;
+    if (k == 0) {
+        let signin = document.createElement("a");
+        signin.setAttribute("class", "sign");
+        signin.href = "login_page.html";
+        signin.innerHTML = `<div>SIGN IN</div>`;
+        login_div.append(signin);
+    } else if (k == 1) {
+        let login = document.createElement("div");
+        login.setAttribute("id", "login");
+        login.innerHTML = `<span class="material-icons">account_circle</span>`;
+        login.addEventListener("click", function () {
+            change_logout();
+        })
+        login_div.append(login);
+        let logout = document.createElement('div');
+        logout.setAttribute("id", "logout");
+        logout.innerHTML = `<p style="display:flex;flex-direction:row;align-items:center;justify-content:center">
+                                <span class="material-icons">logout</span>
+                                    Logout
+                            </p>`;
+        logout.addEventListener('click', function () {
+            logout();
+        })
+        login_div.append(logout)
+    }
+}
+
+login();
+
+function change_logout() {
+    l++;
+    let logout = document.getElementById("logout");
+    if (l % 2 == 1) {
+        logout.style.display = "block";
+    } else {
+        logout.style.display = "none";
+    }
+
+}
+
+function logout() {
+    
 }
